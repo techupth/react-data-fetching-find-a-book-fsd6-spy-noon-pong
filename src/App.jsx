d546c316ee140d7dcaad12074df1e9b5b8a2e825
Pong <pongpong34756@gmail.com>
2024-06-07 16:55:38 +0700

    feat:search engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchText, setSearchText] = useState("");
+  const [searchResualt, setSearchResualt] = useState([]);
+
+  const getItem = async (text) => {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${Text}`
+    );
+    setSearchResualt(result.data);
+  };
+
+  useEffect(() => {
+    if (searchText) {
+      getItem(searchText);
+    }
+  }, [searchText]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <label htmlFor="search-text">search: </label>
+      <input
+        id="search-text"
+        type="text"
+        onChange={(e) => setSearchResualt(e.target.value)}
+      />
+      {searchResualt.length > 0 && (
+        <>
+          {searchResualt.map((result, index) => (
+            <div key={index} className="autocompleteItems">
+              <li className="search-result">{result.volumeInfo.title}</li>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
